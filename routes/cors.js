1b8ec5dd191a26e0ee363c24a39a78aa289b88f3
Gavin D'mello <32271895+eliterudy@users.noreply.github.com>
2023-03-12 23:05:15 -0400

    Update cors.js

--- routes/cors.js
+++ routes/cors.js
@@ -10,9 +10,6 @@ const whitelist = [
   "http://localhost:3002",
   "https://eliterudy.github.io/recipe-diary-frontend",
   "https://eliterudy.github.io/recipe-diary-frontend/"
-  "https://gavin97dmello.github.io/recipe-diary-frontend/",
-  "https://build-my-stack.github.io",
-  "https://gavindmello97.github.io",
 ];
 
 var corsOptionsDelegate = (req, callback) => {
